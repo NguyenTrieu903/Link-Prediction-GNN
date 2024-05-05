33202872c8dd422e787558948f78a6c423c54377
thanhbinh9412002 <nguyenthanhbinhntb9@gmail.com>
2024-05-05 11:28:05 +0700

    add word

--- SEAL/operators/seal_link_predict.py
+++ SEAL/operators/seal_link_predict.py
@@ -25,6 +25,7 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     time_display = st.sidebar.empty()
     status_text.text("0% Complete")
 
+    # Load data tu file
     start_time = time.time()
     positive, negative, nodes_size = data.load_data(is_directed)
     current_step += step_size
@@ -32,13 +33,14 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     progress_bar.progress(current_step / 100) 
     update_time(time_display, start_time, time.time())
 
-
+    # Tao Embedding
     embedding_feature = data.learning_embedding(positive, negative, nodes_size, test_ratio, dimension, is_directed)
     current_step += step_size
     status_text.text("{:.0f}% Complete".format(current_step))
     progress_bar.progress(current_step / 100)  
     update_time(time_display, start_time, time.time())
 
+    # Tao tieu do thi
     start_time = time.time()
     graphs_adj, labels, vertex_tags, node_size_list, sub_graphs_nodes, tags_size = \
         subgraph.link2subgraph(positive, negative, nodes_size, test_ratio, hop, is_directed)
@@ -47,6 +49,7 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     progress_bar.progress(current_step / 100)  
     update_time(time_display, start_time, time.time())
 
+    # Tao 
     start_time = time.time()
     D_inverse, A_tilde, Y, X, nodes_size_list, initial_feature_dimension = data.create_input_for_gnn_fly(
         graphs_adj, labels, vertex_tags, node_size_list, sub_graphs_nodes, embedding_feature, None, tags_size)
@@ -55,6 +58,7 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     progress_bar.progress(current_step / 100)  
     update_time(time_display, start_time, time.time())
 
+    # Chia data
     start_time = time.time()
     D_inverse_train, D_inverse_test, A_tilde_train, A_tilde_test, X_train, X_test, Y_train, Y_test, \
         nodes_size_list_train, nodes_size_list_test = utils.split_train_test(D_inverse, A_tilde, X, Y, nodes_size_list)
@@ -63,6 +67,7 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     progress_bar.progress(current_step / 100)  
     update_time(time_display, start_time, time.time())
 
+    # Xay dung model
     start_time = time.time()
     model = gnn.build_model(top_k, initial_feature_dimension, nodes_size_list_train, nodes_size_list_test,
                             learning_rate, debug=False)
@@ -71,6 +76,7 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     progress_bar.progress(current_step / 100)  
     update_time(time_display, start_time, time.time())
 
+    # Train data
     start_t = time.time()
     gnn.train(model, X_train, D_inverse_train, A_tilde_train, Y_train, nodes_size_list_train, epoch)
     current_step += step_size
@@ -78,6 +84,7 @@ def execute(is_directed, test_ratio, dimension, hop, learning_rate, top_k=60, ep
     progress_bar.progress(current_step / 100)  
     update_time(time_display, start_t, time.time())
 
+    # Prediction result
     #prediction = gnn.predict(model, X_test[0], Y_test[0], A_tilde_test[0], D_inverse_test[0], nodes_size_list_test[0])
     start_time = time.time()
     test_acc, prediction_one, prediction, pos_scores, pos_score_value_one = gnn.predict(model, X_test, Y_test, A_tilde_test, D_inverse_test, nodes_size_list_test)
