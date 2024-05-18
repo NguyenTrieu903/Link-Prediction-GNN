2b6479bd0b7257d6f3800fbf79edb3498fcfa929
NguyenTrieu903 <nguyentrieutptp@gmail.com>
2024-05-18 15:45:46 +0700

    update model

--- TwoWL/TwoWL_work.py
+++ TwoWL/TwoWL_work.py
@@ -6,7 +6,7 @@ import optuna
 import json
 from TwoWL.operators.datasets import load_dataset, dataset
 from TwoWL.model import train
-from TwoWL.model.model import LocalWLNet, WLNet, FWLNet, LocalFWLNet
+from TwoWL.model.model import WLNet
 import streamlit as st
 from assets.theme import update_time
 from constant import *
@@ -16,7 +16,7 @@ import numpy as np
 
 def work(args, device="cpu"):
     global seconds_passed
-    seconds_passed = 0 
+    seconds_passed = 0
 
     total_steps = 4
     step_size = 100 / total_steps
@@ -49,7 +49,7 @@ def work(args, device="cpu"):
 
     current_step += step_size
     status_text.text("{:.0f}% Complete".format(current_step))
-    progress_bar.progress(current_step / 100) 
+    progress_bar.progress(current_step / 100)
     update_time(time_display, start_time, time.time())
 
     def selparam(trial):
@@ -63,49 +63,55 @@ def work(args, device="cpu"):
             val_ds = dataset(*bg.split(1))
             tst_ds = dataset(*bg.split(2))
         lr = trial.suggest_categorical("lr", [0.0005, 0.001, 0.005, 0.01, 0.05])
-        depth1 = trial.suggest_int("l1", 1, 3)
-        depth2 = trial.suggest_int("l2", 1, 3)
-        channels_1wl = trial.suggest_categorical("h1", [24, 32, 64])
-        channels_2wl = trial.suggest_categorical("h2", [16, 24])
-        dp_lin0 = trial.suggest_float("dpl0", 0.0, 0.8, step=0.1)
-        dp_lin1 = trial.suggest_float("dpl1", 0.0, 0.8, step=0.1)
-        dp_emb = trial.suggest_float("dpe", 0.0, 0.5, step=0.1)
-        dp_1wl0 = trial.suggest_float("dp10", 0.0, 0.5, step=0.1)
-        dp_1wl1 = trial.suggest_float("dp11", 0.0, 0.5, step=0.1)
-        dp_2wl = trial.suggest_float("dp2", 0.0, 0.5, step=0.1)
-        act0 = trial.suggest_categorical("a1", [True, False])
-        act1 = trial.suggest_categorical("a2", [True, False])
+        layer1 = trial.suggest_int("layer1", 2, 3)
+        layer2 = trial.suggest_int("layer2", 1, 3)
+        layer3 = trial.suggest_int("layer3", 1, 3)
+        hidden_dim_1 = trial.suggest_categorical("h1", [20])
+        hidden_dim_2 = trial.suggest_categorical("h1", [20])
+        dp0_0 = trial.suggest_float("dp0_0", 0.0, 0.8, step=0.1)
+        dp0_1 = trial.suggest_float("dp0_1", 0.0, 0.8, step=0.1)
+        dp1 = trial.suggest_float("dp1", 0.0, 0.5, step=0.1)
+        dp2 = trial.suggest_float("dp2", 0.0, 0.5, step=0.1)
+        dp3 = trial.suggest_float("dp3", 0.0, 0.5, step=0.1)
+        ln0 = trial.suggest_categorical("ln0", [True, False])
+        ln1 = trial.suggest_categorical("ln1", [True, False])
+        ln2 = trial.suggest_categorical("ln2", [True, False])
+        ln3 = trial.suggest_categorical("ln3", [True, False])
+        ln4 = trial.suggest_categorical("ln4", [True, False])
+        act0 = trial.suggest_categorical("a0", [True, False])
+        act1 = trial.suggest_categorical("a1", [True, False])
+        act2 = trial.suggest_categorical("a2", [True, False])
+        act3 = trial.suggest_categorical("a3", [True, False])
+        act4 = trial.suggest_categorical("a4", [True, False])
         setting = {
-            'dp_lin0': dp_lin0,
-            'dp_lin1': dp_lin1,
-            'dp_emb': dp_emb,
-            'dp_1wl0': dp_1wl0,
-            'dp_1wl1': dp_1wl1,
-            'dp_2wl': dp_2wl,
-            'channels_1wl': channels_1wl,
-            'channels_2wl': channels_2wl,
-            'depth1': depth1,
-            'depth2': depth2,
+            'hidden_dim_1':hidden_dim_1,
+            'hidden_dim_2':hidden_dim_2,
+            'layer1': layer1,
+            'layer2': layer2,
+            'layer3': layer3,
+            'dp0_0': dp0_0,
+            'dp0_1': dp0_1,
+            'dp1': dp1,
+            'dp2': dp2,
+            'dp3': dp3,
+            'ln0': ln0,
+            'ln1': ln1,
+            'ln2': ln2,
+            'ln3': ln3,
+            'ln4': ln4,
             'act0': act0,
             'act1': act1,
+            'act2': act2,
+            'act3': act3,
+            'act4': act4,
             'lr': lr,
         }
-        #return valparam(setting)
         return valparam(setting)
 
     def valparam(kwargs):
         lr = kwargs.pop('lr')
         epoch = args.epoch
-        if args.pattern == '2wl':
-            mod = WLNet(max_degree, use_node_attr, trn_ds.na, **kwargs).to(device)
-        elif args.pattern == '2wl_l':
-            #print("2wl_l")
-            #st.write("2wl_l")
-            mod = LocalWLNet(max_degree, use_node_attr, trn_ds.na, **kwargs).to(device)
-        elif args.pattern == '2fwl':
-            mod = FWLNet(max_degree, use_node_attr, trn_ds.na, **kwargs).to(device)
-        elif args.pattern == '2fwl_l':
-            mod = LocalFWLNet(max_degree, use_node_attr, trn_ds.na, **kwargs).to(device)
+        mod = WLNet(max_degree, use_node_attr, trn_ds.na, **kwargs).to(device)
         opt = Adam(mod.parameters(), lr=lr)
         return train.train_routine("fb-pages-food", mod, opt, trn_ds, val_ds, tst_ds, epoch, verbose=True)
 
@@ -113,7 +119,7 @@ def work(args, device="cpu"):
     study = optuna.create_study(direction='maximize')
     current_step += step_size
     status_text.text("{:.0f}% Complete".format(current_step))
-    progress_bar.progress(current_step / 100) 
+    progress_bar.progress(current_step / 100)
     update_time(time_display, start_time, time.time())
 
     start_time = time.time()
@@ -127,18 +133,17 @@ def work(args, device="cpu"):
         chart.add_rows(new_rows)
         progress_.progress((trial.number + 1) * 10)  # Hiển thị tiến độ trên thanh tiến trình
         time.sleep(0.01)  # Đợi 0.01 giây để mô phỏng quá trình huấn luyện
-    
+
 
     study.optimize(selparam, n_trials=10, callbacks=[lambda study, trial: callback(study, trial)])  # Tối ưu hoá với 100 thử nghiệm
     #best_params = study.best_params
     #progress_.empty()
     current_step += step_size
     status_text.text("{:.0f}% Complete".format(current_step))
-    progress_bar.progress(current_step / 100) 
+    progress_bar.progress(current_step / 100)
     update_time(time_display, start_time, time.time())
 
     start_time = time.time()
-    # Tên tệp nhật ký để lưu trữ thông số
     log_file = "logs.json"
     value_file = "values.json"
 
@@ -151,9 +156,9 @@ def work(args, device="cpu"):
         json.dump(values, f)
     current_step += step_size
     status_text.text("{:.0f}% Complete".format(current_step))
-    progress_bar.progress(current_step / 100) 
+    progress_bar.progress(current_step / 100)
     update_time(time_display, start_time, time.time())
-    
+
     #print("Các thông số tối ưu đã được lưu vào tệp nhật ký:", log_file)
     # st.write("Các thông số tối ưu đã được lưu vào tệp nhật ký:", log_file)
     #st.write(best_params)
@@ -177,25 +182,4 @@ def read_results_twowl():
                 #annotations_auc_twowl = float(time.split(":")[1])
 
     return values, info_values, auc, best_auc_twowl
-
-# if __name__ == "__main__":
-    # import argparse
-    # parser = argparse.ArgumentParser(description='')
-    # parser.add_argument('--dataset', type=str, default="fb-pages-food")
-    # parser.add_argument('--pattern', type=str, default="2wl_l")
-    # parser.add_argument('--epoch', type=int, default=100)
-    # parser.add_argument('--episode', type=int, default=200)
-
-    # parser.add_argument('--seed', type=int, default=0)
-    # parser.add_argument('--device', type=int, default=-1)
-    # parser.add_argument('--path', type=str, default="Opt/")
-    # parser.add_argument('--test', action="store_true")
-    # parser.add_argument('--check', action="store_true")
-    # args = parser.parse_args()
-    # if args.device < 0:
-    #     args.device = "cpu"
-    # else:
-    #     args.device = "cuda:" + str(args.device)
-    # work(args.device)
-    # pass
 
